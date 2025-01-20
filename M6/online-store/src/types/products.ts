ad00017affcd775cdcf7bef71b8103c1e354d046
DanielBrMz <a01254805@tec.mx>
2025-01-20 00:00:00 -0600

    File added: products.ts

---
+++ M6/online-store/src/types/products.ts
@@ -0,0 +1,25 @@
+export interface Product {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  stock: number;
+  weight?: number;
+  rating?: number;
+  createdAt?: Date;
+}
+
+export interface AdminContextType {
+  products: Product[];
+  addProduct: (product: Omit<Product, "id">) => void;
+  updateProduct: (product: Product) => void;
+  deleteProduct: (id: number) => void;
+}
+
+export interface ProductsResponse {
+  data: Product[];
+  totalPages: number;
+  currentPage: number;
+}
