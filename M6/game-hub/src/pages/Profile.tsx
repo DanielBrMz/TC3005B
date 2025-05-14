bfa7b405e2465b7b47ed911581beaeba039dafd6
DanielBrMz <a01254805@tec.mx>
2025-05-14 00:00:00 -0600

    Profile page

---
+++ M6/game-hub/src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div className="profile-page">
+      <h1>Your Profile</h1>
+      <p>Manage your gaming profile</p>
+    </div>
+  );
+};
+
+export default Profile;
