917472b99646db694c41b54a7666c1afb13df68f
DanielBrMz <a01254805@tec.mx>
2025-05-14 00:00:00 -0600

    Home page

---
+++ M6/game-hub/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Game Hub</h1>
+      <p>Discover your next favorite game!</p>
+    </div>
+  );
+};
+
+export default Home;
