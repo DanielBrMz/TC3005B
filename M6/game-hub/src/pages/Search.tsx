955d5e9badb7f86a80f5844be2776e4c57b026c9
DanielBrMz <a01254805@tec.mx>
2025-05-14 00:00:00 -0600

    Search page

---
+++ M6/game-hub/src/pages/Search.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search: React.FC = () => {
+  return (
+    <div className="search-page">
+      <h1>Search Games</h1>
+      <p>Search for your favorite games</p>
+    </div>
+  );
+};
+
+export default Search;
